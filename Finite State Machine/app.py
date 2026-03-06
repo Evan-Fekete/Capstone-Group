48dc63b5e6c1b877d31b51a52164c34f99fe490f
Helix2002 <ronakray2002@gmail.com>
2026-03-06 15:23:34 -0500

    Added some initail centriod logic to app.py

--- Finite State Machine/app.py
+++ Finite State Machine/app.py
@@ -10,10 +10,10 @@ def look_around(find_object):
 
     if find_object in class_names:
         # start webcam
-        
+
         # Uncomment for Linux
         cap = cv2.VideoCapture(0)
-        
+
         # # Uncomment for Windows
         # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -56,7 +56,8 @@ def look_around(find_object):
                     print("Class name -->", class_names[cls])
 
                     # Dimensions
-                    found_result = dimenisons(find_object, class_names[cls], x1, y1, x2, y2)
+                    found_result = dimenisons(
+                        find_object, class_names[cls], x1, y1, x2, y2)
 
                     # object details
                     org = [x1, y1]
@@ -85,29 +86,37 @@ def look_around(find_object):
 def dimenisons(find_object, class_names, x1, y1, x2, y2):
     bounding_x = int(x2) - int(x1)
     bounding_y = int(y2) - int(y1)
+    centriod_x = (int(x2) + int(x1)) / 2
+    center_x = 320
+    centriod_y = (int(y2) + int(y1)) / 2
+    center_y = 240
+
+    offset = abs(centriod_x - center_x)
     print("Find Object: " + find_object + ", Class Names: " + class_names)
     print("Dimenison X of Bounding Box -->", bounding_x)
     print("Dimenison Y of Bounding Box -->", bounding_y)
-    if (find_object == class_names and 355 < bounding_x and 306 < bounding_y):
+    if (find_object == class_names and 355 < bounding_x and 306 < bounding_y and offset < 40):
         print("You are in front of mug")
         return [True, bounding_x, bounding_y]
-    elif (find_object == class_names and 50 < bounding_x and 50 < bounding_y):
+    elif (find_object == class_names and 50 < bounding_x and 50 < bounding_y and offset < 40):
         print("You are in front of apple")
         return [True, bounding_x, bounding_y]
-    elif (find_object == class_names and 217 < bounding_x and 365 < bounding_y):
+    elif (find_object == class_names and 217 < bounding_x and 365 < bounding_y and offset < 40):
         print("You are in front of medicine")
         return [True, bounding_x, bounding_y]
-    elif (find_object == class_names and 265 < bounding_x and 455 < bounding_y):
+    elif (find_object == class_names and 265 < bounding_x and 455 < bounding_y and offset < 40):
         print("You are in front of user")
         return [True, bounding_x, bounding_y]
     else:
         print("No object found")
         return [False, bounding_x, bounding_y]
 
+
 def main():
     with open('object.JSON', 'r') as input:
         query = json.load(input)
     look_around(query.get("object"))
+
 
 if __name__ == "__main__":
     main()
